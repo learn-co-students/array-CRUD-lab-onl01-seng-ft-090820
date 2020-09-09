a907317a38ff08073900217337a71eb6b7aade85
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-09 18:17:51 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,36 +7,29 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_to_end = ["new", "awesome"]
-  add_to_end.push("arrays!")
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  add_to_start = ["surprise", "excited"]
-  add_to_start.unshift("wow")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  remove_from_end = ["boolean", "string", "arrays!"]
-  remove_from_end.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  remove_from_start = ["wow", "passing", "muscle"]
-  remove_from_start.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  random = ["wow", "am", "arrays!"]
-  random[1]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  random = ["wow", "am", "arrays!"]
-  random.first
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  random = ["wow", "am", "arrays!"]
-  random.last
+  array.last
 end
