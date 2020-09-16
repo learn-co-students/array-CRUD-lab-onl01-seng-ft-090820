92963907bb014d29d2f521b76b82b5156f6771d5
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-16 01:58:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,6 +32,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  chocolate = ["Snickers", "Reeses", "Toblerone", "M&Ms"]
-  chocolate.third
+  x = array.num
+  array[x-1]
 end
