d7dc53b2379cd9df13ca4ff882419866408cafd1
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-16 01:56:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,12 +20,11 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  chocolate.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  chocolate = ["Snickers", "Reeses", "Toblerone", "M&Ms"]
-  chocolate[2]
+  array[index.number]
 end
 
 def retrieve_first_element_from_array(array)
