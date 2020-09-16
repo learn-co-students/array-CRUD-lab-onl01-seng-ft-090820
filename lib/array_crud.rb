7ff7af36ace7719b34fb246cc726a4b7cdd48d7f
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-16 01:53:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,7 +16,6 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  chocolate = ["Snickers", "Reeses", "Toblerone", "M&Ms"]
   chocolate.pop
 end
 
