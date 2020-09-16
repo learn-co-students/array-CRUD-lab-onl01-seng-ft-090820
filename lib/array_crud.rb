899f672c1247f213529cf419846be3fc44f09b7f
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-16 01:53:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  chocolate.unshift("Kit Kat")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
