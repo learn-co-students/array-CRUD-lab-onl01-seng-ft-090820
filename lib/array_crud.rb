432d8e260a27ee7a7ace82f461de50b3f22aa693
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-16 01:59:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,8 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  chocolate = ["Snickers", "Reeses", "Toblerone", "M&Ms"]
-    puts "#{chocolate}"
+  ["Snickers", "Reeses", "Toblerone", "M&Ms"]
 end
 
 def add_element_to_end_of_array(array, element)
